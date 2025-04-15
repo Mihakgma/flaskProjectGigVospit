c35dc90cb4281a477efdfdfe79591d0ab66d70b1
Tabakaev Mihail <mihakgma86@hotmail.com>
2025-04-15 10:50:55 +0700

    tryed to use postgreSQL... but lets do it later...

--- config/config.py
+++ config/config.py
@@ -1,9 +1,12 @@
 import os
 
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # UP-LEVEL FROM CURRENT DIR!!!
-db_path = os.path.join(basedir, 'example.db')
+# db_path = os.path.join(basedir, 'example.db')
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{db_path}'
+    # my favourite password...
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # set DATABASE_URL="postgresql://user:password@localhost:5432/example"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
