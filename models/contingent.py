20980333cc0f164ad85935402a04ccc195d0b7fa
Tabakaev Mihail <mihakgma86@hotmail.com>
2025-04-16 14:43:14 +0700

    deleted files from models dir with commented code... one file with models remained there...

--- models/contingent.py
+++
@@ -1,9 +0,0 @@
-# from database import db
-# from sqlalchemy import Text
-# from sqlalchemy.types import String
-#
-#
-# class Contingent(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(String(30), nullable=False)
-#     additional_info = db.Column(Text)
