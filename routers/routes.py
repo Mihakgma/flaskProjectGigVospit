d58f836fba3847d659228730085a2134d81ead3a
Tabakaev Mihail <mihakgma86@hotmail.com>
2025-04-16 15:09:20 +0700

    through poetry added werkzeug. also added new user functionality with html page for it...

--- routers/routes.py
+++ routers/routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, jsonify, request
-from models.models import User  # Импортируем модель User
-from database import db  # Импортируем db
+from flask import Blueprint, render_template, jsonify, request, redirect, url_for
+from models.models import User, Role, Department, Status
+from database import db
+from werkzeug.security import generate_password_hash
 
 routes_bp = Blueprint('routes', __name__)  # Создаем blueprint
 
@@ -21,8 +22,8 @@ def submit():
     return f'Введенные Вами данные: <{name}>'
 
 
-@routes_bp.route('/add_user', methods=['POST'])
-def add_user():
+@routes_bp.route('/new_user', methods=['POST'])
+def new_user():
     try:
         name = request.form['user_name']
         email = request.form['user_email']
@@ -33,3 +34,54 @@ def add_user():
     except Exception as e:
         db.session.rollback()
         return jsonify({'error': str(e)}), 500
+
+
+@routes_bp.route('/users/add', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        try:
+            # получаем все поля из формы
+            first_name = request.form.get('first_name')
+            last_name = request.form.get('last_name')
+            middle_name = request.form.get('middle_name')
+            username = request.form.get('username')
+            email = request.form.get('email')
+            password = request.form.get('password')
+            phone = request.form.get('phone')
+            role_ids = request.form.getlist('role_id')  # получаем список выбранных ролей
+            dept_id = request.form.get('dept_id')
+            status_id = request.form.get('status_id')
+
+            hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+
+            new_user = User(
+                first_name=first_name,
+                last_name=last_name,
+                middle_name=middle_name,
+                username=username,
+                email=email,
+                password=hashed_password,
+                phone=phone,
+                dept_id=dept_id,
+                status_id=status_id
+            )
+
+            # Добавляем роли к пользователю
+            roles = Role.query.filter(Role.id.in_(role_ids)).all()
+            new_user.roles.extend(roles)
+
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('user_routes.add_user'))  # Или другая страница после добавления
+
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({'error': str(e)}), 500
+
+    roles = Role.query.all()
+    departments = Department.query.all()
+    statuses = Status.query.all()
+    return render_template('add_user.html',
+                           roles=roles,
+                           departments=departments,
+                           statuses=statuses)
