6dbd66df9fe4fa5262ce81d6d709ccfe85278d7f
Tabakaev Mihail <mihakgma86@hotmail.com>
2025-04-23 16:31:47 +0700

    department updated with codes in En lang

--- models/models.py
+++ models/models.py
@@ -34,6 +34,7 @@ class Status(db.Model):
 class Department(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    code = db.Column(String(5), nullable=False, unique=True)
 
     def __repr__(self):
         return f'<Department {self.name}>'
