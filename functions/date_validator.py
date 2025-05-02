01f2fd7faf4d6d94e476fbaa020765781f45f896
Tabakaev Mihail <mihakgma86@hotmail.com>
2025-05-03 00:18:48 +0700

    birth date validator has been updated

--- functions/date_validator.py
+++ functions/date_validator.py
@@ -7,5 +7,17 @@ from flask_wtf import FlaskForm
 def validate_birth_date(form: FlaskForm, field):
     """Проверяем, что дата рождения не позже сегодняшнего дня."""
     input_date = field.data
-    if input_date >= date.today():
+    today = date.today()
+    dates_difference = today - input_date
+    dates_difference_years = round(dates_difference.days / 365.25, 0)
+    dates_difference_months = today.month - input_date.month
+    if dates_difference_months < 0:
+        dates_difference_months = 0
+    if input_date >= today:
         raise ValidationError("Дата рождения не должна быть из будущего.")
+    elif input_date < today and dates_difference_years < 14:
+        raise ValidationError(f"Заявителю должно быть как минимум 14 лет, "
+                              f"фактич. ~ <{dates_difference_years}> лет, <{dates_difference_months}> мес.")
+    elif input_date < today and dates_difference_years > 90:
+        raise ValidationError(f"Заявителю пора на заслуженный отдых, "
+                              f"фактич. ~ <{dates_difference_years}> лет, <{dates_difference_months}> мес.")
